e472fe283aa9a7ded8a55a3174e627e248a9e340
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-03-05 13:35:53 +0100

    Update ignoreError runtime plugin to new interface

--- conf/plugins/ignoreError.kts
+++ conf/plugins/ignoreError.kts
@@ -1,15 +1,14 @@
+import helper.OutputCollector
 import helper.Shell.ExecutionException
 import io.vertx.core.Vertx
 import model.processchain.Executable
 import runtime.OtherRuntime
 
-fun ignoreError(executable: Executable, outputLinesToCollect: Int, vertx: Vertx): String {
-  return try {
-    OtherRuntime().execute(executable, outputLinesToCollect)
+fun ignoreError(executable: Executable, outputCollector: OutputCollector, vertx: Vertx) {
+  try {
+    OtherRuntime().execute(executable, outputCollector)
   } catch (e: ExecutionException) {
-    if (e.exitCode == 1) {
-      ""
-    } else {
+    if (e.exitCode != 1) {
       throw e
     }
   }
